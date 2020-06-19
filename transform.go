8fd6b36abef2d87dd6a43b8e5df0c6e6c4fcc675
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2020-06-19 12:18:33 +0800

    add transform  implements

--- transform.go
+++ transform.go
@@ -1,6 +1,10 @@
 package bingo_dao
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+	"unicode"
+)
 
 /*
   数据转换组件
@@ -86,6 +90,171 @@ func (this *FieldMapping) BuildTransform() TransformFunction {
 			targetField = this.Field
 		}
 		input[targetField] = target
+		return input
+	}
+	return t
+}
+
+/**
+  3、增加常亮作为新的字段
+*/
+type AddConstant struct {
+	singleComponse
+	NewField string
+	Type     string
+	Value    string
+}
+
+func (this *AddConstant) BuildTransform() TransformFunction {
+	t := func(input Data) Data {
+		//只有不存在新字段的时候才加入，如果存在该字段则不作处理
+		if _, ok := input[this.NewField]; !ok {
+			input[this.NewField] = this.Value
+		}
+		return input
+	}
+	return t
+}
+
+/**
+  4、字段截取
+*/
+type CutField struct {
+	singleComponse
+	NewField string
+	Field    string
+	Start    int
+	End      int
+}
+
+func (this *CutField) BuildTransform() TransformFunction {
+	t := func(input Data) Data {
+		//只有不存在新字段的时候才加入，如果存在该字段则不作处理
+		if v, ok := input[this.Field]; ok {
+			target := v.(string)
+			target = target[this.Start:this.End]
+			input[this.NewField] = target
+		}
+		return input
+	}
+	return t
+}
+
+/*
+  5、字符串替换
+  * 输入字段
+  * 输出字段
+  * 匹配内容
+  * 替换内容
+  * 启用正则表达式、大小写敏感、整个单词匹配
+  * 设为空串
+*/
+type ReplaceField struct {
+	singleComponse
+	NewField     string
+	Field        string
+	Match        string
+	EnableRegexp bool
+	Replace      string
+	regex        *regexp.Regexp
+}
+
+func (this *ReplaceField) BuildTransform() TransformFunction {
+	t := func(input Data) Data {
+		//只有不存在新字段的时候才加入，如果存在该字段则不作处理
+		if v, ok := input[this.Field]; ok {
+			target := v.(string)
+			if this.EnableRegexp {
+				if this.regex == nil {
+					this.regex, _ = regexp.Compile(this.Match)
+				}
+				target = this.regex.ReplaceAllString(target, this.Replace)
+			} else {
+				target = strings.Replace(target, this.Match, this.Replace, -1)
+			}
+			input[this.NewField] = target
+		}
+		return input
+	}
+	return t
+}
+
+type TrimType byte
+
+const (
+	TT_Left  TrimType = 1 //左边空格
+	TT_Right TrimType = 2 //右边空格
+	TT_Both  TrimType = 3 //两端空格
+)
+
+/**
+  6、字符串去空格
+*/
+type TrimField struct {
+	singleComponse
+	NewField string
+	Field    string
+	Type     TrimType
+}
+
+func (this *TrimField) BuildTransform() TransformFunction {
+	t := func(input Data) Data {
+		if v, ok := input[this.Field]; ok {
+			target := v.(string)
+			switch this.Type {
+			case TT_Left:
+				target = strings.TrimLeftFunc(target, unicode.IsSpace)
+			case TT_Right:
+				target = strings.TrimRightFunc(target, unicode.IsSpace)
+			case TT_Both:
+				target = strings.TrimSpace(target)
+
+			}
+			input[this.NewField] = target
+		}
+		return input
+	}
+	return t
+}
+
+//大小写类型
+type UpperLowType byte
+
+const (
+	CT_Title   UpperLowType = 3 //标题
+	CT_First   UpperLowType = 4 //首字母大写
+	CT_Upper   UpperLowType = 1 //转大写
+	CT_Lowcase UpperLowType = 2 //转小写
+)
+
+type CaptionField struct {
+	singleComponse
+	NewField string
+	Field    string
+	Type     UpperLowType
+}
+
+func (this *CaptionField) BuildTransform() TransformFunction {
+	t := func(input Data) Data {
+		if v, ok := input[this.Field]; ok {
+			target := v.(string)
+			switch this.Type {
+			case CT_Upper:
+				target = strings.ToUpper(target)
+			case CT_Lowcase:
+				target = strings.ToLower(target)
+			case CT_Title:
+				target = strings.ToTitle(target)
+			case CT_First:
+				strArry := []rune(target)
+				if strArry[0] >= 97 && strArry[0] <= 122 {
+					strArry[0] -= 32
+				}
+				target = string(strArry)
+
+			}
+			input[this.NewField] = target
+		}
 		return input
 	}
 	return t
--- transform_test.go
+++ transform_test.go
@@ -21,3 +21,50 @@ func TestFieldMapping_BuildTransform(t *testing.T) {
 	c2 := FieldMapping{NewField: "Nstatus", Field: "status", DefaultValue: "null", Mapping: map[string]string{"1": "TRUE"}}
 	t.Log(c2.BuildTransform()(value))
 }
+
+func TestCutField_BuildTransform(t *testing.T) {
+	value := make(map[string]interface{})
+	value["id"] = "1234"
+	value["j1"] = "abc"
+	value["status"] = "1"
+	c := CutField{NewField: "New", Field: "j1", Start: 0, End: 1}
+	t.Log(c.BuildTransform()(value))
+}
+
+func TestReplaceField_BuildTransform(t *testing.T) {
+	value := make(map[string]interface{})
+	value["id"] = "1234"
+	value["j1"] = "abcdefghijk"
+	c := ReplaceField{NewField: "New", Field: "j1", Match: "abc", Replace: "替换", EnableRegexp: false}
+	t.Log(c.BuildTransform()(value))
+	c1 := ReplaceField{NewField: "New1", Field: "j1", Match: "babc", Replace: "替换", EnableRegexp: false}
+	t.Log(c1.BuildTransform()(value))
+	//使用正则表达式
+	c2 := ReplaceField{NewField: "New2", Field: "j1", Match: "bc?", Replace: "替换", EnableRegexp: true}
+	t.Log(c2.BuildTransform()(value))
+}
+
+func TestTrimField_BuildTransform(t *testing.T) {
+	value := make(map[string]interface{})
+	value["id"] = " 1234 "
+	c := TrimField{NewField: "New", Field: "id", Type: TT_Left}
+	t.Log(c.BuildTransform()(value))
+	c1 := TrimField{NewField: "New1", Field: "id", Type: TT_Right}
+	t.Log(c1.BuildTransform()(value))
+	c2 := TrimField{NewField: "New2", Field: "id", Type: TT_Both}
+	t.Log(c2.BuildTransform()(value))
+}
+
+func TestCaptionField_BuildTransform(t *testing.T) {
+	value := make(map[string]interface{})
+	value["id"] = "abCDefGH"
+	c := CaptionField{NewField: "New", Field: "id", Type: CT_Title}
+	t.Log(c.BuildTransform()(value))
+	c1 := CaptionField{NewField: "New1", Field: "id", Type: CT_Upper}
+	t.Log(c1.BuildTransform()(value))
+	c2 := CaptionField{NewField: "New2", Field: "id", Type: CT_Lowcase}
+	t.Log(c2.BuildTransform()(value))
+
+	c3 := CaptionField{NewField: "New3", Field: "id", Type: CT_First}
+	t.Log(c3.BuildTransform()(value))
+}
